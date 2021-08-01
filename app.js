ea55791d3e35b2b14527aafcca9c9d9d0c7e0df9
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-07-31 17:26:48 -0700

    Initial commit

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
@@ -12,9 +13,21 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(
+  session({
+    secret: 'Our little secret.',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect('mongodb://localhost:27017/userDB', {
   useUnifiedTopology: true,
   useNewUrlParser: true,
+  useCreateIndex: true,
 });
 
 const userSchema = new mongoose.Schema({
@@ -22,7 +35,14 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', function (req, res) {
   res.render('home');
@@ -36,41 +56,9 @@ app.get('/register', function (req, res) {
   res.render('register');
 });
 
-app.post('/register', function (req, res) {
-  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash,
-    });
+app.post('/register', function (req, res) {});
 
-    newUser.save(function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render('secrets');
-      }
-    });
-  });
-});
-
-app.post('/login', function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  User.findOne({ email: username }, function (err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, function (err, result) {
-          if (result === true) {
-            res.render('secrets');
-          }
-        });
-      }
-    }
-  });
-});
+app.post('/login', function (req, res) {});
 
 app.listen(3000, function () {
   console.log('Sever started on port 3000');
